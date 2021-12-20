f148a3c74bd4df4bb3e8b690224ab02a0db3500e
Brian Muturi <muturibrian18@gmail.com>
2021-12-20 22:42:38 +0100

    changes

--- Generation.cs
+++ Generation.cs
@@ -22,13 +22,14 @@ namespace Conway_sGameOfLife
         {
             grid[x, y] = true;
         }
+
         //generating the board after passing no of simulation steps
         private static bool[,] NewGeneration(bool[,] grid)
         {
-            bool[,] newGrid = new bool[20, 20];
-            for (int x = 0; x < 20; x++)
+            bool[,] newGrid = new bool[30, 30];
+            for (int x = 0; x < 30; x++)
             {
-                for (int y = 0; y < 20; y++)
+                for (int y = 0; y < 30; y++)
                 {
                     int AliveNeighbours = GetAliveNeighboursCount(x, y, grid);
                     if (grid[x, y] == true)
@@ -44,11 +45,12 @@ namespace Conway_sGameOfLife
             }
             return newGrid;
         }
+
         //Logic of alive neighbors
         public static int GetAliveNeighboursCount(int x, int y, bool[,] grid)
         {
             int count = 0;
-            int n = 20;
+            int n = 30;
             if (grid[(x - 1 + n) % n, (y - 1 + n) % n] == true)
                 count++;
             if (grid[x, (y - 1 + n) % n] == true)
@@ -67,33 +69,37 @@ namespace Conway_sGameOfLife
                 count++;
             return count;
         }
+
         //Generating the grid
-        public Generation(int x,int y)
+        public Generation()
         {
-            this.grid = new bool[30, 30];
+           
             Console.WriteLine("Welcome to Convay’s Game of Life!");
             Console.Write("How many random alive cells should be placed: ");
             initialAliveCellsCount = Convert.ToInt32(Console.ReadLine());
 
         }
+
         // population density
         public Generation(bool[,] grid, int x, int y)
         {
-            
+            this.grid = grid;
             Random generator = new Random();
             double density = (initialAliveCellsCount * 100) / 400;
             Console.WriteLine(density);
 
-            for (int i = 0; i < density; i++)
+           for (int i = 0; i < density; i++)
             {
-                 x = generator.Next(30);
-                 y = generator.Next(30);
+                // x = generator.Next(30);
+                 // y = generator.Next(30);
                 Putters[generator.Next(Putters.Length)](grid, x, y);
             } 
         }
+
+        //shapes of the cells
         private static void PutGlider(bool[,] grid, int x, int y)
         {
-            int n = 20;
+            int n = 30;
             grid[x, (y - 1 + n) % n] = true;
             grid[(x + 1) % n, y] = true;
             grid[(x - 1 + n) % n, (y + 1) % n] = true;
@@ -102,7 +108,7 @@ namespace Conway_sGameOfLife
         }
         private static void PutBlock(bool[,] grid, int x, int y)
         {
-            int n = 20;
+            int n = 30;
             grid[x, y] = true;
             grid[(x + 1) % n, y] = true;
             grid[x, (y + 1) % n] = true;
@@ -110,7 +116,7 @@ namespace Conway_sGameOfLife
         }
         private static void PutTub(bool[,] grid, int x, int y)
         {
-            int n = 20;
+            int n = 30;
             grid[(x - 1 + n) % n, y] = true;
             grid[(x + 1) % n, y] = true;
             grid[x, (y - 1 + n) % n] = true;
--- Program.cs
+++ Program.cs
@@ -6,7 +6,13 @@ namespace Conway_sGameOfLife
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Generation Pacifique = new Generation();
+            
+            Random Princesse = new Random();
+            int x = Princesse.Next();
+            int y = Princesse.Next();
+            bool[,] mugisha = new bool[30, 30];
+            Generation brian = new Generation(mugisha,x,y);
 
         }
     }
